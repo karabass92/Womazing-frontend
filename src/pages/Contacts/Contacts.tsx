d0e900a69165e5e7d096a32df02b7bf87280be42
karabass92 <shatveryank@gmail.com>
2023-06-05 00:36:28 +0600

    adding contactInfo responsive section in contacts page

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -8,6 +8,20 @@ const Contacts = () => {
         <main className={styles.main}>
             <BreadCrumbs header='Контакты' />
             <YandexMap />
+            <section className={styles.contactInfo}>
+                <div>
+                    <h3>Телефон</h3>
+                    <p>+7 (495) 823-54-12</p>
+                </div>
+                <div>
+                    <h3>E-mail</h3>
+                    <p>info@sitename.com</p>
+                </div>
+                <div>
+                    <h3>Адрес</h3>
+                    <p>г. Москва, 3-я улица Строителей, 25</p>
+                </div>
+            </section>
         </main>
     );
 };
