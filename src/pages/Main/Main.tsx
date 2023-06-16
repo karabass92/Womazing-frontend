dd2264acea9b9df40810af0064854a01e7d08ce4
karabass92 <shatveryank@gmail.com>
2023-06-16 23:55:20 +0600

    main page important block layout

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,49 @@
 import styles from './Main.module.scss';
 
+import important1 from '../../assets/img/main-page/important_1.png';
+import important2 from '../../assets/img/main-page/important_2.png';
+import important3 from '../../assets/img/main-page/important_3.png';
+
 
 const Main = () => {
     return (
         <main className={styles.main}>
-            main
+            <section>slider</section>
+            <section>new collection</section>
+            <h2>Что для нас важно</h2>
+            <section className={styles.importantBlock}>
+                <article className={styles.importantItem}>
+                    <div>
+                        <img src={important1} alt="img" />
+                    </div>
+                    <h3>Качество</h3>
+                    <p>
+                        Наши профессионалы работают на лучшем оборудовании для пошива одежды
+                        беспрецедентного качества
+                    </p>
+                </article>
+                <article className={styles.importantItem}>
+                    <div>
+                        <img src={important2} alt='img' />
+                    </div>
+                    <h3>Скорость</h3>
+                    <p>
+                        Благодаря отлаженной системе в Womazing мы можем отшивать до 20-ти единиц
+                        продукции в наших собственных цехах
+                    </p>
+                </article>
+                <article className={styles.importantItem}>
+                    <div>
+                        <img src={important3} alt="img" />
+                    </div>
+                    <h3>Ответственность</h3>
+                    <p>
+                        Мы заботимся о людях и планете. Безотходное производство и комфортные
+                        условия труда - все это Womazing
+                    </p>
+                </article>
+            </section>
+            <section>dreamteam</section>
         </main>
     );
 };
